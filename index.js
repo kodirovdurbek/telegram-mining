dbcde750e894764e4e9c5d62f4c206c0e0b1bcb5
kodirovdurbek <kodirovdurbek00@gmail.com>
2026-09-26 03:46:04 +0500

    Fix WebSocket import statement formatting

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const http = require('http');
-const WebSocket = require('ws');
+const WebSocket = require('ws'); 
 
 const app = express();
 const server = http.createServer(app);
